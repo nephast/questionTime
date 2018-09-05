cd06d17144fce5b580a5eeaf5b61c820a72ffd9b
Stéphan Gaud <stephgoeseast@gmail.com>
2018-09-05 17:12:15 +0100

    launch route update user db and return correct output according to spec (output construct will be moved to helpers when refactoring)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,4 @@ const launchRoute = require('./launch');
 
 module.exports = {
   launch: launchRoute
-}
+};
--- src/routes/launch.js
+++ src/routes/launch.js
@@ -11,10 +11,40 @@ async (req, res, next) => {
   Question.findOne().skip(random).exec(
     (err, randomQuestion) => {
       req.question = randomQuestion;
-      res.status(200).json({ here: randomQuestion })
       return next()
     })
 }) 
-});
+},
+async (req, res, next) => {
+  // add question obj to array of questions if not already there
+   await User.update(
+     { user_id: req.body.userId, 'questions.question_id': { $ne: req.question.question_id }  },
+     { '$push': { 'questions' : { 'question_id': req.question.question_id, 'answered': false, lastOne: true } } }
+   );
+   // update last question asked so we know later on which one to match the user answer
+   await User.update(
+     { user_id: req.body.userId },
+     { '$set': { 'last_question': req.question.question_id } }
+   );
+  //check if user has already answered this question
+  const alreadyAnswered = await User.find(
+    { user_id: req.body.userId, questions: { '$elemMatch': { 'question_id': req.question.question_id, answered: true } } }
+  );
+    if (alreadyAnswered.length === 0) {
+      //create output answers for question
+      let answers = '';
+      const lastAnswer = req.question.answers.pop();
+      req.question.answers.forEach(answer => {
+        answers+=`${answer.answer_text}, `
+      });
+      answers+=`or ${lastAnswer.answer_text}`
+      res.status(200).json({ output: `<speak>Welcome to Question Time! ${req.question.question} ${answers}</speak>` })
+      return next();
+    } else
+    res.status(200).json({ output: '<speak>Welcome back to Question Time! Sorry, but you\'ve already answered the question</speak>' })
+    return next();
+ 
+}
+);
 
 module.exports = router;
